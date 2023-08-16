9135ab6a2df0eae4e2c66fe9e89e963fee391948
Malympian <136430216+Malympian@users.noreply.github.com>
2023-08-16 07:24:58 -0700

    Update my_cryptography.py

--- my_cryptography.py
+++ my_cryptography.py
@@ -1,111 +1,43 @@
-# my_cryptography.py
-
-# Install the cryptography library if you haven't already
-# (You don't need to include this line in your code)
-# pip install cryptography
-
-# Import necessary modules from the cryptography library
-from cryptography.hazmat.primitives import hashes, padding
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-from cryptography.hazmat.primitives.asymmetric import rsa, padding as asym_padding
-from cryptography.hazmat.primitives import serialization
-
 import os
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 import base64
+import secrets
+import hashlib
+from cryptography.fernet import Fernet
+
+def generate_aes_key():
+    return secrets.token_urlsafe(32)
+
+def hmac_derive_key(password, salt):
+    return hashlib.pbkdf2_hmac('sha256', password, salt, 100000, dklen=32)
+  # Make the iteration number larger for higher security. The CPU hates me if I set to 1m.
 
 def aes_encrypt(data, key):
-    iv = os.urandom(16)
-    cipher = Cipher(algorithms.AES(key), modes.CBC(iv))
-    encryptor = cipher.encryptor()
-    padder = padding.PKCS7(algorithms.AES.block_size).padder()
-    padded_data = padder.update(data) + padder.finalize()
-    encrypted_data = encryptor.update(padded_data) + encryptor.finalize()
-    return iv + encrypted_data
+    salt = os.urandom(16)
+    aes_key = hmac_derive_key(key.encode(), salt)
+    cipher = Fernet(base64.urlsafe_b64encode(aes_key).decode())
+    encrypted_data = cipher.encrypt(data.encode())
+    return base64.urlsafe_b64encode(salt + encrypted_data).decode()
 
 def aes_decrypt(data, key):
-    iv = data[:16]
-    cipher = Cipher(algorithms.AES(key), modes.CBC(iv))
-    decryptor = cipher.decryptor()
-    decrypted_data = decryptor.update(data[16:]) + decryptor.finalize()
-    unpadder = padding.PKCS7(algorithms.AES.block_size).unpadder()
-    unpadded_data = unpadder.update(decrypted_data) + unpadder.finalize()
-    return unpadded_data
+    data = base64.urlsafe_b64decode(data.encode())
+    salt = data[:16]
+    aes_key = hmac_derive_key(key.encode(), salt)
+    cipher = Fernet(base64.urlsafe_b64encode(aes_key).decode())
+    decrypted_data = cipher.decrypt(data[16:])
+    return decrypted_data.decode()
 
-def get_shift(char):
-    return (ord(char.lower()) - ord('a'))
-
-def shift_letter(char, key_char, reverse=False):
-    if char.isalpha():
-        shift = get_shift(key_char)
-        if reverse:
-            shift = -shift
-        ord_value = ord(char.lower()) + shift
-        if char.isupper():
-            if ord_value > ord('z'):
-                ord_value -= 26
-            elif ord_value < ord('a'):
-                ord_value += 26
-            return chr(ord_value).upper()
-        else:
-            if ord_value > ord('z'):
-                ord_value -= 26
-            elif ord_value < ord('a'):
-                ord_value += 26
-            return chr(ord_value)
-    else:
-        return char
-
-def vigenere_encrypt(text, keyword):
-    encrypted = ""
-    keyword = keyword.lower()
-    keyword_index = 0
-
-    for char in text:
-        encrypted += shift_letter(char, keyword[keyword_index])
-        if char.isalpha():
-            keyword_index = (keyword_index + 1) % len(keyword)
-
-    return encrypted
-
-def vigenere_decrypt(text, keyword):
-    decrypted = ""
-    keyword = keyword.lower()
-    keyword_index = 0
-
-    for char in text:
-        shifted = shift_letter(char, keyword[keyword_index], reverse=True)
-        decrypted += shifted
-        if char.isalpha():
-            keyword_index = (keyword_index + 1) % len(keyword)
-
-    return decrypted
-
-def reverse_text(text):
-    return "".join(reversed(text))
-
-def encode(text):
+def encode(text, aes_key=None):
     try:
-        reversed_text = reverse_text(text)
-        vigenere_encrypted = vigenere_encrypt(reversed_text, KEYWORD)
-        aes_key = os.urandom(16)
-        aes_encrypted = aes_encrypt(vigenere_encrypted.encode(), aes_key)
-        aes_encoded_data = aes_key + aes_encrypted
-        return base64.b64encode(aes_encoded_data).decode()
+        if aes_key is None:
+            aes_key = generate_aes_key()
+        aes_encrypted = aes_encrypt(text, aes_key)
+        return aes_key, aes_encrypted
     except Exception as e:
-        raise ValueError("Encryption error: " + str(e))
+        raise ValueError("Error occurred during encoding: " + str(e))
 
-def decode(text):
+def decode(data, key):
     try:
-        aes_encoded_data = base64.b64decode(text.encode())
-        aes_key = aes_encoded_data[:16]
-        aes_encrypted = aes_encoded_data[16:]
-        vigenere_encrypted = aes_decrypt(aes_encrypted, aes_key)
-        reversed_text = vigenere_decrypt(vigenere_encrypted.decode(), KEYWORD)
-        return reverse_text(reversed_text)
+        aes_decrypted = aes_decrypt(data, key)
+        return aes_decrypted
     except Exception as e:
-        raise ValueError("Decryption error: " + str(e))
-
-# Fetch the KEYWORD from the environment variable (Repl.it Secret)
-import os
-KEYWORD = os.getenv("KEYWORD")
+        raise ValueError("Error occurred during decoding: " + str(e))
